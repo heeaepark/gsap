2e7b09ec137fc26a5a0edbf5b9a6b0a57b2f9e89
heeaepark <pha060611@gmail.com>
2022-11-28 16:38:24 +0900

    pin time

--- GSAP-Sandbox/3. Projects/1.StartingProjects/6. Pin Time/app.js
+++ GSAP-Sandbox/3. Projects/1.StartingProjects/6. Pin Time/app.js
@@ -12,3 +12,36 @@ const src = [
     "1792 | AJD",
   ]
   
+  ScrollTrigger.create({
+    trigger: '.sections',
+    pin: '.tooltip',
+    start: 'top 40%',
+    end: 'bottom 55%',
+    /* markers: true */
+  })
+
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section, index) => {
+    ScrollTrigger.create({
+      trigger: section,
+      start: 'top center',
+      end: 'bottom center',
+      markers: true,
+      onEnter: () => {
+        gsap.set('.tooltip img', {
+          attr: {src: src[index]}
+        });
+        gsap.set('.tooltip p', {
+          innerText: dates[index]
+        })
+      },
+      onEnterBack: () => {
+        gsap.set('.tooltip img', {
+          attr: {src: src[index]}
+        });
+        gsap.set('.tooltip p', {
+          innerText: dates[index]
+        })
+      }
+    })
+  })
