ce624a06fb4e913008dc27178cd61c26e042e4cb
heeaepark <pha060611@gmail.com>
2022-11-28 15:16:37 +0900

    resize event add

--- GSAP-Sandbox/2. Scroll Animations/horizontal/app.js
+++ GSAP-Sandbox/2. Scroll Animations/horizontal/app.js
@@ -1,12 +1,8 @@
 const slideWrap = document.querySelector('.slider-wrap');
 const slides = Array.from(document.querySelectorAll('.slide'))
-const slideWrap_wid = slideWrap.offsetWidth;
-const slide_width = slideWrap_wid / slides.length
-console.log(slideWrap_wid);
-console.log(slide_width);
-console.log(slides.length);
+let slideWrap_wid = slideWrap.offsetWidth;
+let window_wid = window.innerWidth;
 
-window.addEventListener('load', horizon);
 
 function horizon() {
   const TL = gsap.timeline({
@@ -22,7 +18,13 @@ function horizon() {
   });
   TL
   .to(".slider-wrap",{
-    x: - (slideWrap_wid - slide_width),
+    x: - (slideWrap_wid - window_wid),
   })
-
 }
+
+window.addEventListener('load', horizon);
+window.addEventListener('resize', () => {
+  slideWrap_wid = slideWrap.offsetWidth;
+  window_wid = window.innerWidth;
+  horizon()
+});
